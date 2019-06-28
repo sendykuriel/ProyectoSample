fc08588382f54bb72e5e0ba5e3136610db76905a
URiel Sendyk <sendykuriel@gmail.com>
2019-06-28 18:25:02 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/pedidos', to: 'static_pages#pedidos'
+  get  '/signup', to: 'users#new'
 end
