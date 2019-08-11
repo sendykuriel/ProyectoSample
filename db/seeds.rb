700d0f7ab38b6b1c543cb64ce718d405bcf412f6
URiel Sendyk <sendykuriel@gmail.com>
2019-08-11 15:04:15 +0000

    Finish user edit, update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create!(name:  "Admin",
+             email: "sendykuriel@gmail.com",
+             password:              "12345678",
+             password_confirmation: "12345678",
+             admin: true,
+             area:"admin")
+             
+             
